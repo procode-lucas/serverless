b41767b536282a06e54e04093e362486c29c2ebd
procode-lucas <81336310+procode-lucas@users.noreply.github.com>
2021-04-29 09:48:49 -0400

    Fixed Security Issue

--- md5sum/index.js
+++ md5sum/index.js
@@ -3,6 +3,16 @@
 const { exec } = require("child_process");
 
 exports.handler = (event, handler, callback) => {
+    
+value = "teststring"
+
+if (!/^[a-z0-9 .-]*$/i.test("teststring")) {
+        console.log('Bad input for ' + input);
+        callback(null, {
+                statusCode: 400,
+                body: "Please provide only letters, numbers, periods, dashes, and spaces"
+        });
+}
 
     //var input = "test"
     var input = event.queryStringParameters.input.trim();
